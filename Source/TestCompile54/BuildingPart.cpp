eb5266f1d9817040299d426c8f47c6bff6e703eb
Tyler Santos <LeAkame4@gmail.com>
2025-11-17 01:10:46 -0500

    Building/Crafting System and Crash Fix

---
+++ Source/TestCompile54/BuildingPart.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BuildingPart.h"
+
+// Sets default values
+ABuildingPart::ABuildingPart()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	PivotArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Pivot Arrow"));
+
+	RootComponent = PivotArrow;
+	Mesh->SetupAttachment(PivotArrow);
+
+}
+
+// Called when the game starts or when spawned
+void ABuildingPart::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABuildingPart::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
